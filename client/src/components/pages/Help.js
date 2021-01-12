fc3f53b785390f84c8a55ee067e99721d07bf1c8
DevFaizaan <faizaan.b247@gmail.com>
2021-01-12 13:52:43 +0000

    formatting

---
+++ client/src/components/pages/Help.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function Help() {
+    return (
+        <div>
+            <h1 className= 'help'>
+                help us
+            </h1>
+
+        </div>
+    )
+}
+
+export default Help
---
+++ client/src/components/pages/What_Can_You_Do.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function What_Can_You_Do() {
+    return (
+        <div>
+            <h1 className= 'What-Can-You-Do'>
+                wcyd
+            </h1>
+
+        </div>
+    )
+}
+
+export default What_Can_You_Do
