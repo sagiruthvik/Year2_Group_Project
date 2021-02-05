db3f41da60169af90428030a86bd18c6e7bbbee7
DevFaizaan <faizaan.b247@gmail.com>
2021-02-05 18:22:21 +0000

    added user login and signup request

---
+++ Back-End/authFAIZAAN/src/main/java/mow/group10/payload/request/SignupRequest.java
@@ -0,0 +1,31 @@
+package mow.group10.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+import javax.validation.constraints.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+ 
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+    private Set<String> role;
+    private Set<String> addressCheck;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+}
