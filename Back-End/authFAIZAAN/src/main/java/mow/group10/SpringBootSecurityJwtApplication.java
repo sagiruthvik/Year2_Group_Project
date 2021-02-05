b2adaf3dfe2913b19f019a3471e32add59b439c6
DevFaizaan <faizaan.b247@gmail.com>
2021-02-05 18:18:30 +0000

    Auth initial

---
+++ Back-End/authFAIZAAN/src/main/java/mow/group10/SpringBootSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package mow.group10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ Back-End/authFAIZAAN/src/main/java/mow/group10/models/ERole.java
@@ -0,0 +1,8 @@
+package mow.group10.models;
+
+public enum ERole {
+    //Different types of roles for thw mow website
+	ROLE_USER,
+    ROLE_RESTAURANT,
+    ROLE_ADMIN
+}
