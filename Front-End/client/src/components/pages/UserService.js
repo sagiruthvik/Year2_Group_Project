7604457a33b93d8873b26354b5f02129e3275104
kieranman <73584354+kieranman@users.noreply.github.com>
2021-02-05 06:17:40 +0000

    updated userservice for update and delete api

--- Front-End/client/src/components/pages/UserService.js
+++ Front-End/client/src/components/pages/UserService.js
@@ -1,13 +1,24 @@
 import axios from 'axios'
 
-const USERS_REST_API_URL = 'http://localhost:8080/api/';
+const USERS_REST_API_URL = 'http://localhost:8080/api';
 class UserService{
     
     getFood(){
         return axios.get(USERS_REST_API_URL+'/browse');
     }
     createFood(food){
-        return axios.post(USERS_REST_API_URL+'/food_add',food);
+
+        return axios.post(USERS_REST_API_URL+'/add_food',food);
+    }
+    getFoodById(foodId){
+
+        return axios.get(USERS_REST_API_URL + '/browse/'+foodId);
+    }
+    updateFood(food,foodId){
+        return axios.put(USERS_REST_API_URL + '/browse/' + foodId, food);
+    }
+    deleteFood(foodId){
+        return axios.delete(USERS_REST_API_URL + '/browse/' + foodId);
     }
 }
 
