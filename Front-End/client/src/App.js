420ea43ce12a844664a2f05044670890a88e7964
kieranman <73584354+kieranman@users.noreply.github.com>
2021-02-13 16:59:29 +0000

    commeneted out some page clashes

--- Front-End/client/src/App.js
+++ Front-End/client/src/App.js
@@ -7,18 +7,18 @@ import Services_Offered from './components/pages/Aboutpages/Services_Offered'
 import What_Can_You_Do from './components/pages/Aboutpages/What_Can_You_Do'
 import Home from './components/pages/Home'
 import Problem from './components/pages/Foodwaste/Problem'
-import Facts from './components/pages/Foodwaste/Facts'
+// import Facts from './components/pages/Foodwaste/Facts'
 import Contribute from './components/pages/Foodwaste/Contribute'
 import Help from './components/pages/Help'
 
 
-import Login from './components/pages/login.js';
-import Signup from './components/pages/signup.js';
-import Browse from './components/pages/browse.js';
-import UpdateFood from './components/pages/updateFood.js';
-import addFood from './components/pages/addFood.js';
-import addCart from './components/pages/addtoCart.js';
-import userCart from './components/pages/userCart.js';
+import Login from './components/pages/Login/login.js';
+import Signup from './components/pages/Signup/signup.js';
+// import Browse from './components/pages/browse.js';
+// import UpdateFood from './components/pages/updateFood.js';
+// import addFood from './components/pages/addFood.js';
+// import addCart from './components/pages/addtoCart.js';
+// import userCart from './components/pages/userCart.js';
 
 import { BrowserRouter as Router , Route, Switch} from 'react-router-dom';
 import Main from './components/pages/Profilepages/Main';
@@ -38,21 +38,21 @@ function App() {
         <Route path='/Services_Offered' exact component = {Services_Offered}/>
         <Route path= '/What_Can_You_Do' exact component = {What_Can_You_Do} />
         <Route path= '/Problem' exact component = {Problem} />
-        <Route path= '/Facts' exact component = {Facts} />
+        {/* <Route path= '/Facts' exact component = {Facts} /> */}
         <Route path= '/Contribute' exact component = {Contribute} />
         <Route path= '/Help' exact component = {Help} />
         <Route path= '/Profile/Orders' exact component = {Main} />
         <Route path= '/T&C' exact component = {TermsConditions} />
         <Route path= '/Ppolicy' exact component = {PrivacyPolicy} />
         <Route path= '/FAQ' exact component = {FAQ} />
-		<Route path = "/browse" exact component ={Browse} />
+		{/* <Route path = "/browse" exact component ={Browse} />
 		<Route path = "/add_food" exact component ={addFood} />
-		<Route path = "/update-food/:id" exact component ={UpdateFood} />
+		<Route path = "/update-food/:id" exact component ={UpdateFood} /> */}
 		<Route path= '/signup' exact component = {Signup} />
 		<Route path= '/login' exact component = {Login} />
 		<Route path= '/notFound' exact component = {notFound} />
-		<Route path= '/cart-food/:id' exact component = {addCart} />
-		<Route path= '/userCart' exact component = {userCart} />
+		{/* <Route path= '/cart-food/:id' exact component = {addCart} />
+		<Route path= '/userCart' exact component = {userCart} /> */}
 		
         
       </Switch>
