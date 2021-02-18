7d8710bbb7b7504594f859831fc0d6f35ef3a321
unknown <1941325@brunel.ac.uk>
2021-02-18 00:59:55 +0000

    overall changes notfound page

--- Front-End/client/src/components/pages/Findfood/notFound.js
+++ Front-End/client/src/components/pages/Findfood/notFound.js
@@ -1,23 +1,25 @@
-
 import React from 'react'
 import Footer from "../../Footer"
 import '../Findfood/notFound.css'
+import img from '../../../img/notFound.jpg'
 
 
 function notFound() {
     return(
-<div className = "bg-fundo">
+<div>
 
-    
+<img className="bg-findfood" src={img}></img>
+
     <div className = "bg-text"> 
-        <h1 >Sorry we are currently unavailable in your area</h1>
+        <h1 className ="unavailable" >Sorry we are currently unavailable in your area</h1>
     </div>   
+    
 
-     <div className ="footer-down">
-         <Footer/>
-     </div>
+     {/* <div className ="footer-down">
         
-     
+     </div> */}
+        
+     <Footer/>
     
 </div>
 
@@ -28,4 +30,3 @@ function notFound() {
 
 
 export default notFound;
-
