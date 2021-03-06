793dc02448b5364c33d2cefc867a3546a9f3aea7
Ruthvik <ruthvik.1202@gmail.com>
2021-03-06 12:04:56 +0530

    worked on updating users info and commented unused files

--- Front-End/client/src/components/pages/Profilepages/pages/Profile.js
+++ Front-End/client/src/components/pages/Profilepages/pages/Profile.js
@@ -4,7 +4,6 @@ import Modal from 'react-modal'
 
 import React, { Component } from 'react';
 import axios from "axios";
-import authHeader from "../../Login/authHeader";
 
 class Profile extends Component {
 
@@ -29,15 +28,14 @@ class Profile extends Component {
 
 
   componentDidMount(){
+    const access_token1 = localStorage.getItem('token');
+    const access_token = access_token1.substring(1, access_token1.length-1);
+    console.log("Token "+access_token);
     const api = 'http://localhost:8080/api/v1/users/';
-    let checkAccessToken = authHeader().token;
-    let accessToken = "";
-    if(checkAccessToken !== undefined) {
-      accessToken = authHeader().token.toString();
-    }
+
     axios.interceptors.request.use(
       config => {
-        config.headers.authorization = accessToken;
+        config.headers.authorization = access_token;
         return config;
       },
       error =>{
@@ -53,6 +51,16 @@ class Profile extends Component {
             });
           })
   }
+
+  postingData(){
+    axios.post("http://localhost:8080/api/v1/users/postData", "Testing the post request").then((res)=>{
+      console.log(res)
+    }).catch((error)=>{
+      console.log(error);
+    })
+  }
+
+
 
   render() {
 
@@ -85,7 +93,7 @@ class Profile extends Component {
                       <input type="number" name="number" className="input_number" defaultValue={ProfileData.phoneNumber}  /><br /><br />
                       <input type="text" name="address" className="input_address" defaultValue={ProfileData.address}  />
                     </form><br /><br />
-                    <button className="update_btn" onClick={()=>alert("Profile Updated", this.closeModal())}>UPDATE</button><br /><br />
+                    <button className="update_btn" onClick={()=>alert("Profile Updated", this.postingData())}>UPDATE</button><br /><br />
                   </div>
                 </Modal>
                 <div className="user_content_details">
@@ -100,51 +108,3 @@ class Profile extends Component {
 }
 
 export default Profile;
-
-// function Profile() {
-//   const[modalIsOpen, setModalIsOpen] = useState(false);
-//     return(
-//         <>
-//     {Profiledata.map((item,index)=>{
-//       return(
-//         <div className="profile_details" key={index}>
-//         <div className="left_content">
-//           {/* <img className="p_img" src={item.image}></img> */}
-//           <div className="user_details">
-//           <p className="p_name">{item.Name}</p><br/>
-//           <div className="mini_details">
-//           <span className="p_location">{item.icon} {item.Address}</span>
-//           <span className="p_number">{item.ph_no}</span>
-//           <span className="p_email">{item.email}</span>
-//           </div>
-//           </div>
-//         </div>
-//         <div className="right_content">
-//           <button className="p_edit_btn" onClick={()=>setModalIsOpen(true)}>Edit Profile</button>
-//           <Modal isOpen={modalIsOpen} onRequestClose={()=>setModalIsOpen(false)}>
-//             <div className="my_modal">
-//               <h1>Edit Profile</h1>
-//               <button className="close_btn" onClick={()=>setModalIsOpen(false)}>X</button>
-//               <form className="my_form">
-//                 <input type="text" name="name" className="input_name" defaultValue={item.Name}/><br /><br />
-//                 <input type="email" name="email" className="input_email" defaultValue={item.email} /><br /><br />
-//                 <input type="number" name="number" className="input_number" defaultValue={item.ph_no}  /><br /><br />
-//                 <input type="text" name="address" className="input_address" defaultValue={item.Address}  />
-//               </form><br /><br />
-//               <button className="update_btn" onClick={()=>alert("Profile Updated", setModalIsOpen(false))}>UPDATE</button><br /><br />
-//             </div>
-//           </Modal>
-//           <div className="user_content_details">
-//           <span className="p_review">Reviews {item.Reviews}</span>
-//           <span>|</span>
-//           <span className="p_order">Orders {item.Orders}</span>
-//           </div>
-//         </div>
-//       </div>
-//       );
-//     })}
-//     </>
-//     );
-// }
-
-// export default Profile;
