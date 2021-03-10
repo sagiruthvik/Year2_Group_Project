78fd806210e250f20c4e2f04edadb1f4e566411d
jahanzaibakajo <1935525@brunel.ac.uk>
2021-03-10 17:44:30 +0000

    some stylistic changes

--- Front-End/client/src/components/pages/contact-us/contact.js
+++ Front-End/client/src/components/pages/contact-us/contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
 
         <form className="form">
             <h1>Contact Form</h1>
-
+            <h2>We here at Meals on Wheels are always here to feed and support you on the go!</h2>
             <label> Name</label>
             <input placeholder="Type in your Name" />
 
@@ -13,7 +13,7 @@ const Contact = () => {
             <input placeholder="Type in your E-mail Address" />
 
             <label> Message</label>
-            <textarea placeholder='How can we help you?'></textarea>
+            <textarea placeholder='How can we help you today?'></textarea>
 
             <button type='submit'>
             Submit
