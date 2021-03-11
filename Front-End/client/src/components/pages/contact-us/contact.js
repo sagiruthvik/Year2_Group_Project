667dcd9a320c41dc0ea948fcb6f4275131e4f98d
jahanzaibakajo <1935525@brunel.ac.uk>
2021-03-11 09:39:19 +0000

    contact form completion

--- Front-End/client/src/components/pages/contact-us/contact.js
+++ Front-End/client/src/components/pages/contact-us/contact.js
@@ -7,9 +7,31 @@ const Contact = () => {
         const [email, setEmail] = useState("");
         const [message, setMessage] = useState("");
 
+        const handleSubmit = (e) =>{ e.preventDefault();
+        
+            db.collection("contacts").add({
+                name: name,
+                email: email,
+                message: message
+            })
+        .then(() => {
+            alert('Message has been submitted');
+        })
+
+        .catch(error => {
+            alert(error.message);
+        })
+
+        setName("");
+        setEmail("");
+        setMessage("");
+         
+
+        };
+
     return(
 
-        <form className="form">
+        <form className="form" onSubmit = {handleSubmit}>
             <h1>Contact Form</h1>
             <h2>We here at Meals on Wheels are always here to feed and support you on the go!</h2>
             <label> Name</label>
